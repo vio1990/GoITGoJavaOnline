59ce17f527031d1976d885ebcf2060d3597bb6be
Ivan Ozerian <vio1990@ukr.net>
2016-03-13 23:55:37 +0200

    module 1 enterprise not finished

--- src/com/ozerian/enterprise/module1/Main.java
+++ src/com/ozerian/enterprise/module1/Main.java
@@ -1,7 +1,28 @@
 package com.ozerian.enterprise.module1;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        List<Integer> arrayList = new ArrayList<>();
+        List<Integer> linkedList = new LinkedList<>();
+
+        int testQuantity = 100;
+        long result = 0;
+        for (int i = 0; i < testQuantity; i++) {
+            arrayList.clear();
+            long startTime = System.nanoTime();
+            for (int j = 0; j < 10000; j++) {
+                arrayList.add(j);
+            }
+            long endTime = System.nanoTime();
+            long duration = endTime - startTime;
+            result += duration;
+        }
+        result = result / testQuantity;
+        System.out.println(result);
     }
 }

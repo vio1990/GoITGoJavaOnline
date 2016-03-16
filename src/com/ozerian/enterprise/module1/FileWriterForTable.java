f506c93ac11defd03d9f8826caef398ae24c2d02
Ivan Ozerian <vio1990@ukr.net>
2016-03-16 14:15:02 +0200

    final version with table

--- src/com/ozerian/enterprise/module1/FileWriterForTable.java
+++ src/com/ozerian/enterprise/module1/FileWriterForTable.java
@@ -6,8 +6,18 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.CharacterCodingException;
 
+/**
+ * File creation ad writing data in it.
+ */
 public class FileWriterForTable {
 
+    /**
+     * Method creates the file in default directory.
+     * Then method writes the data in this file.
+     * @param elementsAmount amount of collections' elements.
+     * @param message data for writing into the file.
+     * @throws IOException
+     */
     public static void fileWriter(int elementsAmount, String message) throws IOException {
         File file = new File("Comparing collections table with " + elementsAmount + " elements.txt");
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {

fad0120ce4e91de2f78d53ffec74d6cc75565c36
Ivan Ozerian <vio1990@ukr.net>
2016-03-24 13:48:23 +0200

    package name change

--- src/com/ozerian/enterprise/module2/Interfaces/Task.java
+++ src/com/ozerian/enterprise/module2/Interfaces/Task.java
@@ -1,23 +1,23 @@
 package com.ozerian.enterprise.module2.interfaces;
 
 /**
- *  The interface with methods for some Task, which wil be
- *  described and implemented.
+ * The interface with methods for some Task, which wil be
+ * described and implemented.
  * @param <T> This describe type parameter of the Task.
  */
 public interface Task<T> {
 
     /**
-     *  Implementation of the task.
+     * Implementation of the task.
      */
     void execute();
+
 
     /**
      * This method returns the result of executed task.
      * @return <T> result some <T>-type.
      */
     T getResult();
-
 
 }
 
